50fe3068a435ad269a3a08b00ad47898c153b22e
李雅珠 <zhuzhu@liyazhu.local>
2025-02-22 20:29:42 +0800

    [commit] 添加首页轮播图的框架 [version] 0.1.0

--- fangjd/lib/Pages/Home.dart
+++ fangjd/lib/Pages/Home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper_3/flutter_swiper_3.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,15 +9,41 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Widget _swiperWidget() {
+    final List<Map> _imageList = [
+      {
+        "url":
+            "https://pic.rmb.bdstatic.com/bjh/news/0fc6b71c59a69f39cf2e1244d10eaedc.jpeg"
+      },
+      {
+        "url":
+            "https://i-blog.csdnimg.cn/blog_migrate/41635df939e6dd13c6d5e2af785d358b.jpeg"
+      },
+      {
+        "url":
+            "https://img1.baidu.com/it/u=1368815763,3761060632&fm=253&fmt=auto&app=138&f=JPEG?w=760&h=434"
+      },
+    ];
+    return AspectRatio(
+      aspectRatio: 2 / 1,
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return Image.network(
+            _imageList[index]["url"],
+            fit: BoxFit.fill,
+          );
+        },
+        itemCount: _imageList.length,
+        pagination: SwiperPagination(),
+        autoplay: true,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ActionChip(
-        label: Text("点击跳转搜索页面"),
-        onPressed: () {
-          Navigator.pushNamed(context, '/search');
-        },
-      ),
+    return ListView(
+      children: [_swiperWidget()],
     );
   }
 }
