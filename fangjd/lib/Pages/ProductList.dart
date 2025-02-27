4472cf81dc8f5f2e610e6b3b164eee0ac2956680
zhuzhu <15210134184@163.com>
2025-02-27 21:17:56 +0800

    [commit] 添加[商品分类]中列表[上拉加载更多]的逻辑 [version] 0.1.0

--- fangjd/lib/Pages/ProductList.dart
+++ fangjd/lib/Pages/ProductList.dart
@@ -1,4 +1,5 @@
 import 'package:fangjd/CommonWidget/LoadingWidget.dart';
+import 'package:fangjd/Models/ProductModel.dart';
 import 'package:fangjd/Services/ScreenAdapter.dart';
 import 'package:flutter/material.dart';
 
@@ -10,10 +11,73 @@ class ProductlistPage extends StatefulWidget {
 }
 
 class _ProductlistPageState extends State<ProductlistPage> {
-
+  // ScaffoldState, 控制侧边栏的显示
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  // 筛选导航栏的索引
   int _selectIndex = 1;
+  // 商品列表的数据
+  List<ProductItemModel> _productList = [];
+  // 滚动视图的控制器
+  final ScrollController _scrollController = ScrollController();
+  // 是否已显示了上拉加载中
+  bool _isShowMore = false;
+  // 是否有更多的数据
+  bool _isHaveMoreData = true;
 
+  @override
+  void initState() {
+    super.initState();
+
+    _scrollController.addListener((){
+      // 内容视图最大的高度
+      double maxScrollExtent = _scrollController.position.maxScrollExtent;
+      // 视图滚动的大小
+      double scrollContentOffY = _scrollController.position.pixels;
+
+      if (scrollContentOffY > maxScrollExtent + Screenadapter.height(40)) {
+        // 超出了内容视图高度的20，就重新上拉加载更多
+        if (_isShowMore == false && _isHaveMoreData == true) {
+          // 未显示
+          _getProductListDataRequest();
+        }
+        
+      }
+    });
+
+    // -----网络请求--------
+    _getProductListDataRequest();
+  }
+
+  // -----网络请求--------
+  void _getProductListDataRequest() async {
+    setState(() {
+      _isShowMore = true;
+    });
+    // 模拟网络延迟
+    await Future.delayed(Duration(seconds: 2)); 
+    List<ProductItemModel> mockDataList = _mockData();
+    setState(() {
+      _productList.addAll(mockDataList);
+      _isShowMore = false;
+      if (_productList.length > 20) {
+        _isHaveMoreData = false;
+      }
+    });
+  }
+
+  // 模拟数据
+  List<ProductItemModel> _mockData() {
+    // return
+    List<ProductItemModel> mockList = [];
+    for (var index in [1,2,3,4,5,6,7,8,9,0]) {
+      var model = ProductItemModel(iId: 1, title: '笔记本电脑$index', price: "￥3980", oldPrice: "￥4999", pic: "https://www.itying.com/images/flutter/hot${index+1}.jpg");
+      mockList.add(model);
+    }
+    return mockList;
+  }
+
+  // -----视图设置--------
+  // 设置 筛选透视图的底部选中的线
   Border _selectHeaderBoder() {
     return Border(
       bottom: BorderSide(
@@ -107,83 +171,104 @@ class _ProductlistPageState extends State<ProductlistPage> {
     );
   }
 
+  // 商品列表底部的视图
+  Widget _productBottomWiget(int index) {
+    if (_isHaveMoreData == true) {
+      if (_isShowMore && (index == _productList.length -1)) {
+        return Loadingwidget();
+      } else {
+        return Text("");
+      }
+    } else {
+      if (index == _productList.length -1) {
+        return Padding(padding:EdgeInsets.only(top: Screenadapter.height(30)), child: Text("--我也是有底线的--", textAlign: TextAlign.center,));
+      } else {
+        return SizedBox(height: 1);
+      }
+    }
+  }
+
   // 商品列表
   Widget _productListWidget() {
-    return Loadingwidget();
-    /*
-    return  Container(
-      padding: EdgeInsets.all(Screenadapter.width(10)),
-      margin: EdgeInsets.only(top: Screenadapter.height(80)),
-      child: ListView.builder(
-        itemBuilder: (BuildContext context, int index) {
-          return Column(
-            children: [
-              Row(
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(Screenadapter.width(20)),
-                    width: Screenadapter.width(220),
-                    height: Screenadapter.height(220),
-                    child: AspectRatio(
-                      aspectRatio: 1,
-                      child: Image.network('https://www.itying.com/images/flutter/hot5.jpg', fit: BoxFit.cover)
+    if (_productList.isNotEmpty) {
+      return  Container(
+        padding: EdgeInsets.all(Screenadapter.width(10)),
+        margin: EdgeInsets.only(top: Screenadapter.height(80)),
+        child: ListView.builder(
+          controller: _scrollController,
+          itemBuilder: (BuildContext context, int index) {
+            ProductItemModel itemModel = _productList[index];
+            return Column(
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(Screenadapter.width(20)),
+                      width: Screenadapter.width(220),
+                      height: Screenadapter.height(220),
+                      child: AspectRatio(
+                        aspectRatio: 1,
+                        child: Image.network(itemModel.pic, fit: BoxFit.cover)
+                      ),
                     ),
-                  ),
-                  Expanded(child: Container(
-                    height: Screenadapter.height(220),
-                    padding: EdgeInsets.only(top: Screenadapter.height(10)),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "艾伦伯顿（Alen Botun）跑步运动套装男宽松晨跑速干衣体育田径骑行训练马拉松健身房冬季 五件套-四季搭配 XL",
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                        SizedBox(height: Screenadapter.height(20)),
-                        Container(
-                          height: Screenadapter.height(36),
-                          width: double.infinity,
-                          child: Row(
-                            children: [
-                              Container(
-                                height: Screenadapter.height(40),
-                                width: Screenadapter.width(80),
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(Screenadapter.height(18)),
-                                  color: Colors.black12
-                                ),
-                                child: Center(child: Text(' 4G ', style: TextStyle(color: Colors.black87))),
-                              ),
-                              SizedBox(width: Screenadapter.width(20)),
-                              Container(
-                                height: Screenadapter.height(40),
-                                width: Screenadapter.width(80),
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(Screenadapter.height(18)),
-                                  color: Colors.black12
-                                ),
-                                child: Center(child: Text(' 126 ', style: TextStyle(color: Colors.black87))),
-                              ),
-                            ],
+                    Expanded(child: Container(
+                      height: Screenadapter.height(220),
+                      padding: EdgeInsets.only(top: Screenadapter.height(10)),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            itemModel.title,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
                           ),
-                        ),
-                        SizedBox(height: Screenadapter.width(20)),
-                        Text("￥189", style: TextStyle(color: Colors.red, fontSize: 18),)
-                      ]
-                    )
-                  ))
-                ],
-              ),
-              Divider(height: Screenadapter.height(2))
-            ],
-          );
-        },
-        itemCount: 10
-      ),
-    );
-    */
+                          SizedBox(height: Screenadapter.height(20)),
+                          Container(
+                            height: Screenadapter.height(36),
+                            width: double.infinity,
+                            child: Row(
+                              children: [
+                                Container(
+                                  height: Screenadapter.height(40),
+                                  width: Screenadapter.width(80),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(Screenadapter.height(18)),
+                                    color: Colors.black12
+                                  ),
+                                  child: Center(child: Text(' 4G ', style: TextStyle(color: Colors.black87))),
+                                ),
+                                SizedBox(width: Screenadapter.width(20)),
+                                Container(
+                                  height: Screenadapter.height(40),
+                                  width: Screenadapter.width(80),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(Screenadapter.height(18)),
+                                    color: Colors.black12
+                                  ),
+                                  child: Center(child: Text(' 126 ', style: TextStyle(color: Colors.black87))),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: Screenadapter.width(20)),
+                          Text(itemModel.price, style: TextStyle(color: Colors.red, fontSize: 18))
+                        ]
+                      )
+                    ))
+                  ],
+                ),
+                Divider(height: Screenadapter.height(2)),
+                _productBottomWiget(index)
+              ],
+            );
+          },
+          itemCount: _productList.length
+        ),
+      );
+    } else {
+      return Loadingwidget();
+    }
   }
   
   @override
